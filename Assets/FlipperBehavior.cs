19a1e15786d23bd144bc7d05fa26691be0f62f2d
Markus Lindberg <markus.lindberg073@gmail.com>
2018-01-22 12:31:36 +0200

    init
    Initial commit for project

---
+++ Assets/FlipperBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperBehavior : MonoBehaviour {
+    public float restPosition = 0f;
+    public float pressedPosition = 45f;
+    public float hitStrength = 10000f;
+    public float flipDamper = 150f;
+
+    public string inputName;
+
+    HingeJoint hinge;
+	// Use this for initialization
+	void Start () {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipDamper;
+
+        if (Input.GetAxis(inputName) == 1)
+        {
+            spring.targetPosition = pressedPosition;
+
+        }
+        else {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+
+        
+		
+	}
+}
