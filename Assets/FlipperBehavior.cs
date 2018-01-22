4a5f40dc959b5482d37b7e8d0e8df27102a95dc3
Markus Lindberg <markus.lindberg073@gmail.com>
2018-01-22 16:39:48 +0200

    Comments
    Added descriptions to flipper behavior

--- Assets/FlipperBehavior.cs
+++ Assets/FlipperBehavior.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlipperBehavior : MonoBehaviour {
+    // Publics visible in unity
     public float restPosition = 0f;
     public float pressedPosition = 45f;
     public float hitStrength = 10000f;
     public float flipDamper = 150f;
-
+    // Name for the pressed key
     public string inputName;
-
+    // Joint of the flipper
     HingeJoint hinge;
 	// Use this for initialization
 	void Start () {
+        // Retreive the hinge joint attached to flipper paddle
+
         hinge = GetComponent<HingeJoint>();
+        // Set it using a spring
         hinge.useSpring = true;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // Create a spring and set strength and damper to it
         JointSpring spring = new JointSpring();
         spring.spring = hitStrength;
         spring.damper = flipDamper;
-
+        // If a key is pressed, move the paddle to pressed position, otherwise move the paddle to rest position
         if (Input.GetAxis(inputName) == 1)
         {
             spring.targetPosition = pressedPosition;
@@ -32,6 +37,8 @@ public class FlipperBehavior : MonoBehaviour {
         else {
             spring.targetPosition = restPosition;
         }
+        // Set the newly defined spring for the hinge and set it using limits.
+
         hinge.spring = spring;
         hinge.useLimits = true;
 
