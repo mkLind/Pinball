1c9b8981da993de296fecce07e7b7c048daabf42
thilanne <thilanne@abo.fi>
2018-01-25 02:03:30 +0200

    Added initial functionality for triangle bumpers and bouncer cluster

---
+++ Assets/Scripts/BouncerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncerBehaviour : MonoBehaviour {
+
+	public float force = 50.0f;
+
+	private float directionZ;
+
+	//When the ball collides with the bouncer this method is called
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.GetComponent<Rigidbody> () != null) 
+		{
+			//Force to a random direction in the X-axis is added to the 
+			//collision component that has a rigidbody (the ball)
+			directionZ = Random.Range(-1f, 1f);
+			col.GetComponent<Rigidbody>().AddForce (new Vector3 (directionZ, 0, 0) * force);
+		}
+	}
+}
