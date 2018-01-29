72dc844779a895f2b3bf79c674c11f44e6861201
Markus Lindberg <markus.lindberg073@gmail.com>
2018-01-29 16:58:35 +0200

    Story Trigger
    Added capability to read story from the XML file. Resource Folder created and it contains the Story.

--- Assets/Scripts/StoryTrigger.cs
+++ Assets/Scripts/StoryTrigger.cs
@@ -1,28 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
 
 public class StoryTrigger : MonoBehaviour {
   
     public Rect windowR = new Rect(100,50,2000,1000);
     public bool triggered;
+    public string followingPromptId;
+    public string followingPrompt;
+    XmlNodeList list;
+    public string Text;
+    public List<string> options;
+    
+
 
 	// Use this for initialization
 	void Start () {
         // Position the window and set the story element to un triggered state
         windowR = new Rect((Screen.width/2)-200, 50, 400, 100);
         triggered = false;
-	}
+        followingPromptId = "s1";
+
+
+        string loadFrom = Application.dataPath + @"/Resources/Story.XML"; // ÅAth for loading the STORY xml
+        XmlDocument doc = new XmlDocument();// XML DOCUMENT OBJECT
+        string content = System.IO.File.ReadAllText(loadFrom); // Read the contents of the XML file
+
+        doc.LoadXml(content);// Load the xml
+        list = doc.GetElementsByTagName("prompt"); // Allthe promtp elements read.
+        // COntainers for changing options ant text
+        Text = "";
+        options = new List<string>();
+        
+    }
 	
 // Event handling. Add buttons, their shapes and what they do
     void DoMyWindow(int windowID) {
-        if (GUI.Button(new Rect(150, 30, 100, 20), "RUN")) {
+        FetchText(followingPromptId);
+
+        if (GUI.Button(new Rect(150, 30, 100, 20), options[0])) {
             Time.timeScale = 1; // Enable rendering
             triggered = false; // Set to untriggered statew
+            // Clear text and options 
+            Text = "";
+            options = new List<string>();
 
-        } else if (GUI.Button(new Rect(150, 60, 100, 20), "Fight")) {
+        } else if (GUI.Button(new Rect(150, 60, 100, 20), options[1])) {
             Time.timeScale = 1;
             triggered = false;
+
+            Text = "";
+            options = new List<string>();
         }
 
     }
@@ -33,7 +64,7 @@ public class StoryTrigger : MonoBehaviour {
    void OnGUI()
     {
         if (triggered) {
-            windowR = GUI.Window(0, windowR, DoMyWindow, "You encountered a troll. What do you do?");
+            windowR = GUI.Window(0, windowR, DoMyWindow, Text);
         }
 
     }
@@ -52,9 +83,61 @@ public class StoryTrigger : MonoBehaviour {
 
         
     }
-    void OnTriggerExit(Collider other)
-    {
+
+
+
+     void FetchText(string id) {
+
+        XmlNodeList promptData;
+        // Find the promt with right Id
+        foreach (XmlNode data in list) {
+            if (data.Attributes["id"].Value == id) {
+               promptData = data.ChildNodes; // Read all of the children of the prompt element
+
+                // Loop through all the children
+                foreach (XmlNode text in promptData)
+                {
+                    // Fetch the next text
+                    if (text.Name == "text") {
+                        Text = text.InnerText;
+                        
+
+
+
+
+                    }
+                    // Fetch button options
+                    if (text.Name == "option") {
+                        options.Add(text.InnerText);
+
+                    }
+                    // Fetch the next story element Id.
+                    if (text.Name == "next") {
+                        followingPromptId = text.InnerText;
+
+                    }
+
+
+
+
+                }
+                
+
+
+
+
+
+            }
+
+
+        }
+
        
+
+
+
+
+
     }
 
 }
