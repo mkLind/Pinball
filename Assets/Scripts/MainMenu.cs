6f40f7a22bff48b5c66727f04d48294f66eee4a5
thilanne <thilanne@abo.fi>
2018-02-07 16:45:20 +0200

    Highscore, pause + end screen and volume controls

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
@@ -8,6 +9,7 @@ public class MainMenu : MonoBehaviour {
 	//Function referenced in menu's play-button
 	public void PlayGame ()
 	{	
+		//PlayerPrefs.SetInt ("highscore", 77);		//testing
 		//Change scene name for testing different ones
 		SceneManager.LoadScene ("Tommi");
 	}
