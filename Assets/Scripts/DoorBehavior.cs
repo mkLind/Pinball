f936dd571ddb5a9824b1636d34bc5055a6335e7f
Markus Lindberg <markus.lindberg073@gmail.com>
2018-02-07 11:00:27 +0200

    Door behavior
    Better script for door behavior

--- Assets/Scripts/DoorBehavior.cs
+++ Assets/Scripts/DoorBehavior.cs
@@ -8,41 +8,66 @@ public class DoorBehavior : MonoBehaviour {
     public Rigidbody door;
     public float change;
     public bool scaling;
+    public bool closed;
+    public bool open;
     public float scaleFactor;
 
-	// Use this for initialization
-	void Start () {
-		bll = GameObject.Find("Ball").GetComponent<BallBehaviour>();
+    // Use this for initialization
+    void Start() {
+        bll = GameObject.Find("Ball").GetComponent<BallBehaviour>();
         door = GetComponent<Rigidbody>();
-        openLimit = 100;
+
         scaling = false;
+        closed = true;
+        open = false;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (bll.getScore() >= openLimit) {
+
+    // Update is called once per frame
+    void Update() {
+        if (bll.getScore() >= openLimit && closed) {
             scaling = true;
 
-            if (door.transform.localScale.y >= 0.5f) { 
-            scaleFactor = -0.1f;
-        } else if (door.transform.localScale.y == 0f) {
-            scaleFactor = 0.1f;
-        }
-            
+
 
 
 
         }
 
+
+        openDoor();
+
+    }
+
+    void openDoor() {
         if (scaling) {
-            door.transform.localScale -= new Vector3(0, scaleFactor, 0);
 
-            if (door.transform.localScale.y >= 0.5f || door.transform.localScale.y == 0f) {
-                scaling = false;
-            }
+            door.transform.position += new Vector3(0, -0.1f, 0);
+
+
+        }
+        if (door.position.y <= -0.5f) {
+            scaling = false;
+            open = true;
+            closed = false;
         }
 
-		
-	}
+    }
+
+    void closeDoor() {
+        if (scaling)
+        {
+
+            door.transform.position += new Vector3(0, 0.1f, 0);
+
+
+        }
+        if (door.position.y >= 0.7f)
+        {
+            scaling = false;
+            open = false;
+            closed = true;
+        }
+
+    }
 }
