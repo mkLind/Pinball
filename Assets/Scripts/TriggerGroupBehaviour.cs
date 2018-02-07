14065c16adec5cc0b20af8d65add1e396290ec63
meidec <mikko.raula@gmail.com>
2018-02-07 22:02:22 +0200

    Added ABCTrigger to the story

--- Assets/Scripts/TriggerGroupBehaviour.cs
+++ Assets/Scripts/TriggerGroupBehaviour.cs
@@ -15,13 +15,21 @@ public class TriggerGroupBehaviour : MonoBehaviour {
     Transform cylinders;
     // if each cylinder is on or off
     bool[] states;
+    bool taskActive;
+    bool taskCompleted;
+    // this list contains the pattern in which the triggers should be hit to complete goal
+    // for example [2, 1, 0] means the triggers should be hit in that order to succeed
+    // if any other trigger is hit in between, the progress is reset
+    int[] goal;
+    int currentGoalIndex;
 
     void Start()
     {
         cylinders = transform;
         states = new bool[cylinders.childCount];
+        taskActive = false;
 
-        for(var i = 0; i < states.Length; i++)
+        for (var i = 0; i < states.Length; i++)
         {
             Debug.Log(cylinders.GetChild(i));
             Debug.Log(states[i]);
@@ -32,13 +40,6 @@ public class TriggerGroupBehaviour : MonoBehaviour {
     {
     }
 
-    /*
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("parent laughs tooooo");
-        collision.gameObject.GetComponent<Material>().color = Color.red;
-    }
-    */
 
     void ComponentCollided(GameObject child)
     {
@@ -48,11 +49,67 @@ public class TriggerGroupBehaviour : MonoBehaviour {
 
         var objectIndex = child.transform.GetSiblingIndex();
 
-        states[objectIndex] = !states[objectIndex];
-        if (states[objectIndex])
-            material.color = Color.cyan;
+        // if ABC trigger task is active, need to use different rules
+        if (taskActive)
+        {
+            if (objectIndex == currentGoalIndex)
+            {
+                material.color = Color.cyan;
+                states[objectIndex] = true;
+                // check if task goal was reached
+                if (currentGoalIndex + 1 == goal.Length)
+                {
+                    taskCompleted = true;
+                    taskActive = false;
+                    ResetTriggers();
+                } else
+                {
+                    currentGoalIndex++;
+                }
+            }
+            else
+            {
+                ResetTriggers();
+                currentGoalIndex = 0;
+            }
+        }
+        // if ABC trigger task is not active
         else
-            material.color = Color.red;
+        {
+            states[objectIndex] = !states[objectIndex];
+            if (states[objectIndex])
+                material.color = Color.cyan;
+            else
+                material.color = Color.red;
+        }
+    }
+
+    public void ResetTriggers()
+    {
+        for (var i = 0; i < states.Length; i++)
+        {
+            states[i] = false;
+            cylinders.GetChild(i).gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    public void SetTaskActive(int[] goal)
+    {
+        this.goal = goal;
+        ResetTriggers();
+        taskActive = true;
+        taskCompleted = false;
+    }
+
+    public bool isTaskCompleted()
+    {
+        return taskCompleted;
+    }
+
+    // returns the index of how far along the goal the player is
+    public int GetTaskProgress()
+    {
+        return currentGoalIndex;
     }
     
 }
--- Assets/Scripts/VariableContainer.cs
+++ Assets/Scripts/VariableContainer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +8,8 @@ public class VariableContainer : MonoBehaviour {
     public StoryTrigger trig;
     public GameObject[] triggers;
     public BallBehaviour bll;
+    public TriggerGroupBehaviour abcTriggerGroup;
+    public char[] abcTriggers;
    
     public string task = "";
     public string cond = "";
@@ -19,10 +22,14 @@ public class VariableContainer : MonoBehaviour {
 	void Start () {
         trig = GameObject.Find("StoryElement").GetComponent<StoryTrigger>();
         triggers = GameObject.FindGameObjectsWithTag("storypad");
-
+        abcTriggerGroup = GameObject.Find("ABCTriggerGroup").GetComponent<TriggerGroupBehaviour>();
+        // these are used to refer the index of each trigger
+        abcTriggers = new char[] { 'A', 'B', 'C' };
 
         bll = GameObject.Find("Ball").GetComponent<BallBehaviour>();
 
+        print("bll " + bll);
+        print("abc  " + abcTriggerGroup);
 
         tasktext.text = "Task: Hit a story panel."; // Set initial task text
        
@@ -32,10 +39,6 @@ public class VariableContainer : MonoBehaviour {
     public void SetTaskAndCond(string task, string cond) {
         this.task = task;
         this.cond = cond;
-
-
-
-
     }
 
 	void Update () {
@@ -51,28 +54,30 @@ public class VariableContainer : MonoBehaviour {
                 targetScore = currentScore + bumpCond;
                 taskActive = true;
                 bll.setTaskActive();
-
-
-                // Disable story triggers
-
-                for (int i = 0; i<triggers.Length;i++) {
-                    Debug.Log("Disabled triggers: " + i);
-                    triggers[i].GetComponent<StoryTrigger>().disable();
-                    triggers[i].GetComponent<Renderer>().material.color = Color.black;
-
-                }
-
-
-                
-                
             }
 
-     
+            else if (task == "ABCtriggers")
+            {
+                tasktext.text = "Task: Hit the ABC triggers in the following order: " + cond + ". Progress: "; // set the task text to indicate score challenge
+                // turn the condition (= string) into a int array
+                int[] goal = new int[cond.Length];
+                for (var i = 0; i < cond.Length; i++)
+                {
+                    goal[i] = (Array.IndexOf(abcTriggers, cond.ToCharArray()[i]));
+                }
+                abcTriggerGroup.SetTaskActive(goal);
+                taskActive = true;
+                bll.setTaskActive();
+            }
 
+            // Disable story triggers
+            for (int i = 0; i < triggers.Length; i++)
+            {
+                Debug.Log("Disabled triggers: " + i);
+                triggers[i].GetComponent<StoryTrigger>().disable();
+                triggers[i].GetComponent<Renderer>().material.color = Color.black;
 
-
-
-
+            }
         }
         // if task is active in ball, update task
         if (bll.taskStatus()) {
@@ -83,33 +88,39 @@ public class VariableContainer : MonoBehaviour {
                 currentScore = bll.getScore();
                 // Check task condition, if met reset task data
                 if (currentScore >= targetScore ) {
-                    task = "";
-                    cond = "";
-                    tasktext.text = "Task: Hit a story panel.";
-                    taskActive = false;
-                    bll.disableTask();
-                    currentScore = 0;
-                    targetScore = 0;
-                    // enable story triggers
-                    for (int i = 0; i < triggers.Length; i++)
-                    {
-                        triggers[i].GetComponent<StoryTrigger>().setEnabled();
-                        triggers[i].GetComponent<Renderer>().material.color = Color.white;
-                    }
-
-                   
-                   
+                    disableTask();
                 }
-
-
             }
+            else if (task == "ABCtriggers")
+            {
+                // get the current progress
+                var progress = cond.Substring(0, abcTriggerGroup.GetTaskProgress());
+                tasktext.text = "Task: Hit the ABC triggers in the following order: " + cond + ". Progress: " + progress;
 
-
-
-
+                // check if the task is completed
+                if (abcTriggerGroup.isTaskCompleted())
+                {
+                    disableTask();
+                }
+            }
         }
-
-
-		
 	}
+
+    // used when a task has been completed and a new one is ready to be accepted
+    private void disableTask()
+    {
+        task = "";
+        cond = "";
+        tasktext.text = "Task: Hit a story panel to continue with the story.";
+        taskActive = false;
+        bll.disableTask();
+        currentScore = 0;
+        targetScore = 0;
+        // enable story triggers
+        for (int i = 0; i < triggers.Length; i++)
+        {
+            triggers[i].GetComponent<StoryTrigger>().setEnabled();
+            triggers[i].GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
 }
